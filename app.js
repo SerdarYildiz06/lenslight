e4a6bbeec3abb63b8869fff99e2cd3940eab1ee7
Serdar Yildiz <serdaryildizdev@gmail.com>
2024-01-16 17:16:19 +0300

    added model and controller

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import conn from "./db.js";
+import pageRoute from "./routes/pageRoute.js";
+import photoRoute from "./routes/photoRoute.js";
 
 dotenv.config();
 
@@ -8,7 +10,6 @@ dotenv.config();
 conn();
 
 const app = express();
-
 const port = process.env.PORT;
 
 //ejs template engine
@@ -17,9 +18,9 @@ app.set("view engine", "ejs");
 //static files middleware
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+//routes
+app.use("/", pageRoute);
+app.use("/photos", photoRoute);
 
 app.get("/about", (req, res) => {
   res.render("about");
