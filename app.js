b68562d1d1bbf4545c4d59ba8f065176e5720223
Serdar Yildiz <serdaryildizdev@gmail.com>
2024-01-21 15:49:46 +0300

    update

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import conn from './db.js';
-import pageRoute from './routes/pageRoute.js';
-import photoRoute from './routes/photoRoute.js';
-import userRoute from './routes/userRoute.js';
+import express from "express";
+import dotenv from "dotenv";
+import conn from "./db.js";
+import pageRoute from "./routes/pageRoute.js";
+import photoRoute from "./routes/photoRoute.js";
+import userRoute from "./routes/userRoute.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -14,17 +15,20 @@ const app = express();
 const port = process.env.PORT;
 
 //ejs template engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //static files middleware
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+/// cookie parser
+app.use(cookieParser());
+
 //routes
-app.use('/', pageRoute);
-app.use('/photos', photoRoute);
-app.use('/users', userRoute);
+app.use("/", pageRoute);
+app.use("/photos", photoRoute);
+app.use("/users", userRoute);
 
 app.listen(port, () => {
   console.log(`Application running on port: ${port}`);
