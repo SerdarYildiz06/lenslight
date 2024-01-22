fd66a8a6e710c2099345d508d6f72109674641e6
Serdar Yildiz <serdaryildizdev@gmail.com>
2024-01-22 17:03:14 +0300

    fixed auth

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -20,4 +20,9 @@ const getLoginPage = (req, res) => {
     link: "login",
   });
 };
-export { getIndexPage, getAboutPage, getRegisterPage, getLoginPage };
+
+const getLogout = (req, res) => {
+  res.cookie("jwt", "", { maxAge: 1 });
+  res.redirect("/");
+};
+export { getIndexPage, getAboutPage, getRegisterPage, getLoginPage, getLogout };
